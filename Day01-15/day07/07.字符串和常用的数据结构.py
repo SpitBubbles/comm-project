a678b35718bdfaed13215dbb5cd97811e8f0a456
yaojie <yaojie@163.com>
2025-02-07 17:02:09 +0800

    commit

--- Day01-15/day07/07.字符串和常用的数据结构.py
+++ Day01-15/day07/07.字符串和常用的数据结构.py
@@ -16,63 +16,59 @@ s1 = '\'hello, world!\''
 s2 = '\n\\hello, world!\\\n'
 print(s1, s2, end='')
 
-
 #   1.3 在\后面还可以跟一个八进制或者十六进制数来表示字符，
 #       例如\141和\x61都代表小写字母a，前者是八进制的表示法，后者是十六进制的表示法。
 #       也可以在\后面跟Unicode字符编码来表示字符。
 s1 = '\141\142\143\x61\x62\x63'
 print(s1)
 
-
 #   1.4 如果不希望字符串中的\表示转义，我们可以通过在字符串的最前面加上字母r来加以说明。
 s1 = r'\'hello, world!\''
 s2 = r'\n\\hello, world!\\\n'
 print(s1, s2, end='')
 
-
 # 2. 字符串的运算
 #   2.1 Python为字符串类型提供了非常丰富的运算符，我们可以使用+运算符来实现字符串的拼接，可以使用*运算符来重复一个字符串的内容，
 #       可以使用in和not in来判断一个字符串是否包含另外一个字符串（成员运算），我们也可以用[]和[:]运算符从字符串取出某个字符或某些字符（切片运算）。
 s1 = 'hello ' * 3
-print(s1) # hello hello hello
+print(s1)  # hello hello hello
 s2 = 'world'
 s1 += s2
-print(s1) # hello hello hello world
-print('ll' in s1) # True
-print('good' in s1) # False
+print(s1)  # hello hello hello world
+print('ll' in s1)  # True
+print('good' in s1)  # False
 str2 = 'abc123456'
 # 从字符串中取出指定位置的字符(下标运算)
-print(str2[2]) # c
+print(str2[2])  # c
 # 字符串切片(从指定的开始索引到指定的结束索引)
-print(str2[2:5]) # c12
-print(str2[2:]) # c123456
-print(str2[2::2]) # c246
-print(str2[::2]) # ac246
-print(str2[::-1]) # 654321cba
-print(str2[-3:-1]) # 45
-
+print(str2[2:5])  # c12
+print(str2[2:])  # c123456
+print(str2[2::2])  # c246
+print(str2[::2])  # ac246
+print(str2[::-1])  # 654321cba
+print(str2[-3:-1])  # 45
 
 # 2.2 其他
 str1 = 'hello, world!'
 # 通过内置函数len计算字符串的长度
-print(len(str1)) # 13
+print(len(str1))  # 13
 # 获得字符串首字母大写的拷贝
-print(str1.capitalize()) # Hello, world!
+print(str1.capitalize())  # Hello, world!
 # 获得字符串每个单词首字母大写的拷贝
-print(str1.title()) # Hello, World!
+print(str1.title())  # Hello, World!
 # 获得字符串变大写后的拷贝
-print(str1.upper()) # HELLO, WORLD!
+print(str1.upper())  # HELLO, WORLD!
 # 从字符串中查找子串所在位置
-print(str1.find('or')) # 8
-print(str1.find('shit')) # -1
+print(str1.find('or'))  # 8
+print(str1.find('shit'))  # -1
 # 与find类似但找不到子串时会引发异常
 # print(str1.index('or'))
 # print(str1.index('shit'))
 # 检查字符串是否以指定的字符串开头
-print(str1.startswith('He')) # False
-print(str1.startswith('hel')) # True
+print(str1.startswith('He'))  # False
+print(str1.startswith('hel'))  # True
 # 检查字符串是否以指定的字符串结尾
-print(str1.endswith('!')) # True
+print(str1.endswith('!'))  # True
 # 将字符串以指定的宽度居中并在两侧填充指定的字符
 print(str1.center(50, '*'))
 # 将字符串以指定的宽度靠右放置左侧填充指定的字符
@@ -88,3 +84,21 @@ str3 = '  test@126.com '
 print(str3)
 # 获得字符串修剪左右两侧空格之后的拷贝
 print(str3.strip())
+
+
+# 3. 变量的作用域
+def fun():
+    # 在函数中的局部变量（local variable），属于局部作用域，在foo函数的外部并不能访问到它
+    b = 'hello'
+    def fun2():
+        # bar函数中的变量c属于局部作用域，在bar函数之外是无法访问的。
+        c = True
+        print(a)
+        print(b)
+        print(c)
+    fun2()
+
+if __name__ == '__main__':
+    # if分支中定义了一个变量a，这是一个全局变量（global variable），属于全局作用域
+    a = 100
+    fun()
