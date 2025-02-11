8a9fce92d1f0377fb8dc05becf9f4db92f95a905
yaojie <yaojie@163.com>
2025-02-11 18:47:18 +0800

    commit

--- Day01-15/day08/Test.py
+++ Day01-15/day08/Test.py
@@ -8,15 +8,20 @@ class Test:
         print('__bar')
 
 
+
 # 属性和方法的访问权限有两种，公开的,私有的。
 # 如果希望属性是私有的，在给属性命名时可以用两个下划线作为开头
 def main():
-    test = Test('hello')
-    # AttributeError: 'Test' object has no attribute '__bar'
-    test.__bar()
-    # AttributeError: 'Test' object has no attribute '__foo'
-    print(test.__foo)
 
+    # test = Test('hello')
+    # AttributeError: 'Test' object has no attribute '__bar'
+    # test.__bar()
+    # AttributeError: 'Test' object has no attribute '__foo'
+    # print(test.__foo)
+
+    test = Test('hello')
+    test._Test__bar()
+    print(test._Test__foo)
 
 if __name__ == "__main__":
     main()
